f6454f9c207e67241cbc60d0a1598ffa34b29d56
mattpap <mattpap@40396586-b6e1-11dd-8f2b-43a3dbd57a2c>
2008-12-10 23:27:02 +0000

    Zaktualizowane zostały klasy modułu metadanych.

--- trunk/src/pl/pwr/odwa/server/metadata/MetaElement.java
+++ trunk/src/pl/pwr/odwa/server/metadata/MetaElement.java
@@ -1,6 +1,49 @@
 package pl.pwr.odwa.server.metadata;
 
-public class MetaElement
-{
+/** Klasa bazowa dla klas będących elementami bazy metadanych.
+ *
+ *  Każdy element bazy metadanych cechuje się trzema podstawowymi
+ *  atrybutami:
+ *
+ *   [1] Unikalne ID.
+ *   [2] Przyjazna dla urzytkownika nazwa.
+ *   [3] Opis przeznaczenia danego elementu bazy metadanych.
+ *
+ *  Zawartość poszczególnych atrybutów może być ustalana jedynie w
+ *  module metadaych, natomias moduły korzystające z elementów bazy
+ *  metadanych (głównie moduł GUI) może jedynie czytać zawartość
+ *  atrybutów.
+ *
+ * @author Mateusz Paprocki
+ * @author Maria Łabaziewicz
+ */
+public class MetaElement {
+    protected int m_id;        // XXX: int -> MetaID
+    protected String m_name;
+    protected String m_notes;
 
+    protected void setID(int id) {
+        m_id = id;
+    }
+
+    protected void setName(String name) {
+        m_name = name;
+    }
+
+    protected void setNotes(String notes) {
+        m_notes = notes;
+    }
+
+    public int getID() {
+        return m_id;
+    }
+
+    public String getName() {
+        return m_name;
+    }
+
+    public String getNotes() {
+        return m_notes;
+    }
 }
+
