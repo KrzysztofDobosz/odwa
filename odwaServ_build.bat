26e886ecc455a3f3432184acfcf1135b845fca2e
m.spiczak <m.spiczak@40396586-b6e1-11dd-8f2b-43a3dbd57a2c>
2009-01-22 00:28:18 +0000

    odwaServ update: odwaServ_build.bat script added. run and visit http://localhost:8080/odwaServ/odwa

---
+++ odwaServ_build.bat
@@ -0,0 +1,20 @@
+#!/bin/bash
+set TOMCAT_HOME=c:\eclipse\apache-tomcat-6.0.18
+set APPDIR=%cd%
+
+
+javac -cp "%TOMCAT_HOME%\lib\servlet-api.jar;%APPDIR%\odwaServlet;%APPDIR%\odwaServlet\out\WEB-INF\lib\mysql-connector-java-5.1.7-bin.jar;%APPDIR%\odwaServlet\out\WEB-INF\lib\odwa.jar" -d "%APPDIR%\odwaServlet\out\WEB-INF\classes\odwaServlet" odwaServlet\Odwa.java odwaServlet\BasicDom.java
+
+
+copy %APPDIR%\opt\metadata.xml %TOMCAT_HOME%\bin
+
+cd odwaServlet\out
+7z a odwaServ.zip
+
+copy odwaServ.zip %TOMCAT_HOME%\webapps\odwaServ.war
+
+cd %APPDIR%\odwaServlet\out
+del odwaServ.zip
+
+cd %TOMCAT_HOME%\bin
+startup.bat
