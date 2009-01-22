f91374afafbf8d9cce44b9dbea21656367845fdc
m.spiczak <m.spiczak@40396586-b6e1-11dd-8f2b-43a3dbd57a2c>
2009-01-22 15:43:08 +0000

    update servlet script.

--- trunk/odwaServ_build.bat
+++ trunk/odwaServ_build.bat
@@ -4,7 +4,7 @@ set APPDIR=%cd%
 
 rd /s %TOMCAT_HOME%\webapps\odwaServ
 rd %TOMCAT_HOME%\webapps\odwaServ
-javac -cp "%TOMCAT_HOME%\lib\servlet-api.jar;%APPDIR%\odwaServlet;%APPDIR%\odwaServlet\out\WEB-INF\lib\mysql-connector-java-5.1.7-bin.jar;%APPDIR%\odwaServlet\out\WEB-INF\lib\odwa.jar" -d "%APPDIR%\odwaServlet\out\WEB-INF\classes" odwaServlet\Odwa.java odwaServlet\BasicDom.java
+javac -cp "%TOMCAT_HOME%\lib\servlet-api.jar;%APPDIR%\odwaServlet;%APPDIR%\odwaServlet\out\WEB-INF\lib\mysql-connector-java-5.1.7-bin.jar;%APPDIR%\odwaServlet\out\WEB-INF\lib\odwa.jar" -d "%APPDIR%\odwaServlet\out\WEB-INF\classes" odwaServlet\Odwa.java odwaServlet\MetaXmlParserServlet.java
 
 
 copy %APPDIR%\opt\metadata.xml %TOMCAT_HOME%\bin
