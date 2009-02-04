bceccf0d9aa11b62cb2eb1f145e10fdb07492fae
m.spiczak <m.spiczak@40396586-b6e1-11dd-8f2b-43a3dbd57a2c>
2009-02-04 13:22:26 +0000

    Unused, but commented - reports.

--- trunk/src/org/pwr/odwa/client/reports/DynamicReport.java
+++ trunk/src/org/pwr/odwa/client/reports/DynamicReport.java
@@ -6,8 +6,9 @@ import org.pwr.odwa.client.visualization.ReportStyle;
 import org.pwr.odwa.common.selection.UserSelection;
 
 /**
- * Klasa raportu dynamicnzego rozszerzająca abstrakcyjną klasę {@link Report}
- *
+ * 
+ * DynamicReport class extends Report class ({#ling Report})
+ * 
  * @author Katarzyna Rzerzicha
  * @author Michał Brzeziński-Spiczak
  */
@@ -16,66 +17,58 @@ public class DynamicReport extends Report {
 	UserSelection selection;
 	ReportStyle format;
 
-	/**
-	 * Konstruktor domyślny
-	 */
 	public DynamicReport() {
 	};
+
 	/**
-	 * Konstruktor raportu dynamicznego na bazie zapytania do bazy danych
-	 * {@link UserSelection} oraz stylu formatowania {@link ReportStyle}.
-	 *
-	 * @param selection
-	 *            - wynik zapytania do bazy danych
-	 * @param format
-	 *            - styl formatowania
+	 * Constructor of dynamic report baed on User Selection
+	 * {@link UserSelection} and report style {@link ReportStyle}.
 	 */
 	public DynamicReport(UserSelection selection, ReportStyle format) {
 		System.out.println("ODWAClient: DynamicReport: constructor executed");
-	   this.selection = selection;
+		this.selection = selection;
 		this.format = format;
-		//description.add("Date = " + new Date().toString());
-		//description.add("Author = " + "");
+		// description.add("Date = " + new Date().toString());
+		// description.add("Author = " + "");
 	}
+
 	/**
-	 * Odczyt raportu dynamicznego z pliku o nazwie (ścieżce) podanej jako
-	 * argument
-	 *
-	 * @param filepath
-	 *            - nazwa/ścieżka do pliku raportu do odczytu
+	 * Read the report from file
 	 */
 	public DynamicReport(String filepath) {
-		System.out.println("ODWAClient: DynamicReport: filepath constructor executed");
-	   /*try {
-			FileInputStream file = new FileInputStream(filepath);
-			ObjectInputStream objectstream = new ObjectInputStream(file);
-			Object report = objectstream.readObject();
-			if (report instanceof DynamicReport) {
-				DynamicReport rep = (DynamicReport) report;
-				this.description = rep.description;
-				this.selection = rep.selection;
-				this.format = rep.format;
-			}
-
-		} catch (IOException e) {
-			System.out.println("Error in opening report " + e);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}*/
+		System.out
+				.println("ODWAClient: DynamicReport: filepath constructor executed");
+		/*
+		 * try { FileInputStream file = new FileInputStream(filepath);
+		 * ObjectInputStream objectstream = new ObjectInputStream(file); Object
+		 * report = objectstream.readObject(); if (report instanceof
+		 * DynamicReport) { DynamicReport rep = (DynamicReport) report;
+		 * this.description = rep.description; this.selection = rep.selection;
+		 * this.format = rep.format; }
+		 * 
+		 * } catch (IOException e) {
+		 * System.out.println("Error in opening report " + e); } catch
+		 * (ClassNotFoundException e) { e.printStackTrace(); }
+		 */
 	}
+
 	/**
-	 * Zwraca zapisane w raporcie zapytanie użytkownika {@link UserSelection}
+	 * Get user selection from report {@link UserSelection}
 	 */
 
 	public UserSelection getUserSelection() {
-	   System.out.println("ODWAClient: DynamicReport: getUserSelection executed");
+		System.out
+				.println("ODWAClient: DynamicReport: getUserSelection executed");
 		return selection;
 	}
+
 	/**
-	 * Ustawia zapytanie użytkownika {@link UserSelection}
-	 * @param selection - zapytanie użytkowinka
+	 * Set user selection {@link UserSelection}
+	 * 
+	 * @param selection
+	 *            - zapytanie użytkowinka
 	 */
-	public void setUserSelection(UserSelection selection){
+	public void setUserSelection(UserSelection selection) {
 		this.selection = selection;
 	}
 
